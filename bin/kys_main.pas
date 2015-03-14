cef8111ada0f14903ec89ce158e28f2a3e6f46c5
cd <cd@2361c462-ec5a-1946-8964-ff0cdbb1caab>
2015-03-14 17:46:32 +0000

    git-svn-id: https://svn.mythkast.net/svn/pig3@110 2361c462-ec5a-1946-8964-ff0cdbb1caab

--- bin/kys_main.pas
+++ bin/kys_main.pas
@@ -1190,7 +1190,7 @@ begin
   if Result then
   begin
     //if SIMPLE <> 0 then
-    Name := CP936ToUTF8(Simplified2Traditional(UTF8ToCP936(Name)));
+    //Name := CP936ToUTF8(Simplified2Traditional(UTF8ToCP936(Name)));
     str2 := UTF8Decode(Name);
     p0 := @Rrole[0].Name;
     p1 := @str2[1];
