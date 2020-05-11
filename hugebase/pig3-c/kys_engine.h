fade5ed70995aa2a60aa09a5a91d7b947079035a
sty <scarsty@gmail.com>
2020-05-11 23:52:38 +0800

    pig3 pure c

---
+++ hugebase/pig3-c/kys_engine.h
@@ -0,0 +1,20 @@
+#include "kys_type.h"
+
+int DrawUText(wchar_t *text, int x_pos, int y_pos, Uint32 color, int engwidth);
+int DrawShadowText(wchar_t *text, int x_pos, int y_pos, Uint32 color1, Uint32 color2);
+
+char *ReadFileToBuffer(char *p, char *filename, int size, int needmalloc, int *filesize);
+void FreeFileBuffer(char *p);
+int LoadPNGTiles(char *path, PNGIndex **P, SDL_Texture ***T, SDL_Surface ***S, int LoadPic);
+void LoadOnePNGTexture(char *path, char *p, PNGIndex *P, int forceLoad);
+int DrawPNGTile(SDL_Renderer *render, PNGIndex P, int FrameNum,
+                int px, int py, SDL_Rect *region, int shadow, int alpha, Uint32 mixColor, int mixAlpha,
+                double scalex, double scaley, double angle, SDL_Point *center);
+void GetRGBA(Uint32 color, byte *r, byte* g, byte *b, byte *a);
+
+void UpdateAllScreen();
+
+void CleanKeyValue();
+void CheckBasicEvent();
+
+//void
